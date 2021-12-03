765ce3052300ea607fb6fb66a078fa57be2c4fa7
Welisson Silveira <welissonmwse@gmail.com>
2021-12-03 19:53:19 -0300

    resoluçao teste 02

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface SideBarProps{
+  genres: Array<{
+    id: number,
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family',
+    title: string
+  }>,
+  onHandleClickButton: (id: number) => void,
+  selectedGenreId: number,
+
+}
+
+export function SideBar({genres, onHandleClickButton, selectedGenreId}: SideBarProps) {
+
+  return(
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => onHandleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
